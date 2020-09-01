84fa55f66f1be4ee34146faa37b644cb332f900c
Supra-MS <suprabharathi.m.s@gmail.com>
2020-08-31 21:04:59 -0700

    Removed HostProfile & proxy folders

--- HostProfile/client/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import HostInfo from './components/HostInfo';
-require('./style.css');
-
-ReactDOM.render(<HostInfo />, document.getElementById('hostInfo'));
-
