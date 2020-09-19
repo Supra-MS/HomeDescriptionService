9668100d2bf9c2a7ae19b216bff0eff049bd74cf
Supra-MS <suprabharathi.m.s@gmail.com>
2020-09-18 18:36:56 -0700

    Change ec2 instance - storage exceeded

--- client/src/components/HomeDescription.js
+++ client/src/components/HomeDescription.js
@@ -6,7 +6,7 @@ import Description from './sub-components/Description';
 import Amenities from './sub-components/Amenities';
 import SleepingArrangements from './sub-components/SleepingArrangements';
 
-var serverUrl = 'http://ec2-3-101-118-169.us-west-1.compute.amazonaws.com/:3000';
+var serverUrl = 'http://ec2-13-56-20-100.us-west-1.compute.amazonaws.com:3000';
 // var serverUrl = 'http://localhost:3002';
 
 class HomeDescription extends React.Component {
