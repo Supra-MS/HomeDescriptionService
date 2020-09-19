f6c12b0ced4bad259dabc33e7c6696b2945755df
Supra-MS <suprabharathi.m.s@gmail.com>
2020-09-18 20:27:02 -0700

    small changes

--- client/src/components/HomeDescription.js
+++ client/src/components/HomeDescription.js
@@ -7,7 +7,6 @@ import Amenities from './sub-components/Amenities';
 import SleepingArrangements from './sub-components/SleepingArrangements';
 
 var serverUrl = 'http://ec2-13-56-20-100.us-west-1.compute.amazonaws.com:3000';
-var hostServerUrl = 'http://ec2-13-56-20-100.us-west-1.compute.amazonaws.com:3006';
 // var serverUrl = 'http://localhost:3002';
 
 class HomeDescription extends React.Component {
